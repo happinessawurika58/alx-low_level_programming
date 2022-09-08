a591e1f3318c1f620a4e7af022d0d8278afd6281
happiness58 <happinessawurika58@gmail.com>
2022-09-08 15:22:31 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-*  * main - Prints sized of characters
-*   *
-*    *Return: Always 0 (success)
-*     */
+* main - Prints sized of characters
+*
+*Return: Always 0 (success)
+*/
 int main(void)
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
